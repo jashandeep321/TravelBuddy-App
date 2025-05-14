5e98263fa7d823a6b8c3fa3b01480cdb8a8467ce
Chetan Anand <canand2003@gmail.com>
2025-05-14 21:22:19 +0530

    Home-StackWrapper_bug-fixes

--- TravelBuddy/Components/Home.jsx
+++ TravelBuddy/Components/Home.jsx
@@ -1,16 +1,25 @@
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Button } from "react-native";
 import React from "react";
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native';
 import HeroSec from "./homesegments/HeroSec";
 import DestinationSec from "./homesegments/DestinationSec";
 import CatagorySec from "./homesegments/CatagorySec";
+
 const Home = () => {
+  const navigation = useNavigation();
 
   return (
     <ScrollView contentContainerStyle={styles.scrollContainer}>
       <HeroSec />
       <DestinationSec />
-      <CatagorySec/>
+      <CatagorySec />
+      <View style={styles.buttonContainer}>
+        <Button 
+          title="View All Packages" 
+          onPress={() => navigation.navigate('Packages')}
+        />
+      </View>
+
       <Text style={styles.footerText}>Home</Text>
     </ScrollView>
   );
@@ -20,7 +29,15 @@ export default Home;
 
 const styles = StyleSheet.create({
   scrollContainer: {
-
-    backgroundColor:"white"
+    backgroundColor: "white",
+    paddingBottom: 20,
+  },
+  buttonContainer: {
+    margin: 20,
+  },
+  footerText: {
+    textAlign: 'center',
+    marginTop: 20,
+    color: 'gray',
   },
 });
--- TravelBuddy/Components/StackWrapper.jsx
+++ TravelBuddy/Components/StackWrapper.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Welcome from './Welcome';
-import Auth0Login from './user/Auth0Login';
+import Login from './user/Login';
 import AllDestinations from './AllDestinations';
 import Nav from './Tabbar/Nav';
 import Itinerary from './Itinerary';
+import Auth0Login from './user/Auth0Login';
+import Wishlist from './Wishlist';
+import Packages from './Packages';
+import PackageDetailsScreen from './PackageDetails';
 
 const Stack = createStackNavigator();
 
@@ -14,12 +18,15 @@ const StackWrapper = () => {
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName="Auth0Login"
-        screenOptions={{ headerShown: false }}>
+        screenOptions={{headerShown: false}}>
         {/* <Stack.Screen name="Welcome" component={Welcome} /> */}
         <Stack.Screen name="Auth0Login" component={Auth0Login} />
         <Stack.Screen name="Nav" component={Nav} />
         <Stack.Screen name="AllDestinations" component={AllDestinations} />
         <Stack.Screen name="Itinerary" component={Itinerary} />
+        <Stack.Screen name="Wishlist" component={Wishlist} />
+        <Stack.Screen name="Packages" component={Packages} />
+        <Stack.Screen name="PackageDetails" component={PackageDetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
