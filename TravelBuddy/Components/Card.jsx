7c23a0bfe7c9b83850657399505ab42376de0ca5
Ishan Chaudhary <ishanch08@gmail.com>
2025-05-14 12:47:43 +0530

    Fetched data from beckend API to: Alldestination.jsxAdded a Slug page.

---
+++ TravelBuddy/Components/Card.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import Slug from './Slug';
+
+const Card = ({destination}) => {
+  const navigation = useNavigation();
+
+  //   console.log(destination);
+
+  return (
+    <TouchableOpacity
+      style={styles.card}
+      onPress={() => navigation.navigate('Slug', {destination})}>
+      {/* pass data to Slug.jsx */}
+      <Image source={{uri: destination.baseImages[0]}} style={styles.image} />
+      <Text style={styles.title}>{destination.name}</Text>
+      <Text style={styles.subtitle}>{destination.location}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    marginBottom: 15,
+    borderRadius: 10,
+    backgroundColor: '#f9f9f9',
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    padding: 10,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  subtitle: {
+    fontSize: 14,
+    color: 'gray',
+  },
+});
+
+export default Card;
--- TravelBuddy/Components/StackWrapper.jsx
+++ TravelBuddy/Components/StackWrapper.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Welcome from './Welcome';
 import Login from './user/Login';
 import AllDestinations from './AllDestinations';
 import Nav from './Tabbar/Nav';
 import Itinerary from './Itinerary';
+import Auth0Login from './user/Auth0Login';
+import Slug from './Slug';
 
 const Stack = createStackNavigator();
 
 const StackWrapper = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Auth0Login" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator
+        initialRouteName="Auth0Login"
+        screenOptions={{headerShown: false}}>
         {/* <Stack.Screen name="Welcome" component={Welcome} /> */}
         <Stack.Screen name="Auth0Login" component={Auth0Login} />
         <Stack.Screen name="Nav" component={Nav} />
         <Stack.Screen name="AllDestinations" component={AllDestinations} />
         <Stack.Screen name="Itinerary" component={Itinerary} />
+        <Stack.Screen name="Slug" component={Slug} />
       </Stack.Navigator>
     </NavigationContainer>
   );
